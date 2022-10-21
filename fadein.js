fe82b53c929bab1922e9cfa3c648d395d52f8122
fattyray <slimrays@163.com>
2022-10-21 23:07:28 +0800

    111

--- fadein.js
+++ fadein.js
@@ -1,8 +1,8 @@
 fadein=document.getElementById('fadein')
-s="I'm a college student of University of Electronic Science and Technology of China(Also known as uestc 成电)\n" +
+s="I'm a college student of University of Electronic Science and Technology of China(Also known as UESTC)\n" +
     "                Though majoring in SCSE(School of Computer Science and Engineering,I don't really know much about it before\n" +
-    "                I started my freshmen year.And this homepaage or blog is a reminder to me to mark what I have accomplished or\n" +
-    "                to share the interesting things that I wanna share with all of you who cares enough to visit me at this homepage."
+    "                I started my freshman year.And this homepaage or blog is a reminder to me to mark what I have accomplished or\n" +
+    "                to share the interesting things that I wanna share with all of you who care enough to visit me at this homepage."
 nums=s.length
 ni=0
 add=function () {
